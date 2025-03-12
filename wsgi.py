6bb37df0a0c4897a4afbdf903ecadfcddf9ab55b
doxme999 <adelaide.boes0@gmail.com>
2025-03-12 19:31:33 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,4 @@
-import os
-import zipfile
-from app import app  # Assuming your Flask app is in app.py and named 'app'
-
-# Unzip static files (if they exist)
-zip_file_path = 'Anonymous.zip'  # Adjust if the file name is different
-
-if os.path.exists(zip_file_path):
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall('./') # Extracts to the current directory
-    os.remove(zip_file_path) # remove zip file after extraction to save space
+from app import app
 
 if __name__ == "__main__":
     app.run()
